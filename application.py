d18f37977f85c947f1d6abae8d8859c7a1abcbab
SakshamAgarwal007 <sakshamagrawal608@gmail.com>
2023-04-11 23:31:46 +0530

    adding new file

--- application.py
+++ application.py
@@ -4,7 +4,8 @@ import pandas as pd
 from flask import Flask , request, render_template, jsonify
 from sklearn.preprocessing import StandardScaler
 
-app = Flask(__name__)
+application = Flask(__name__)
+app = application
 
 #import ridge regressor model and standard scaler pickle
 ridge_model = pickle.load(open('models/ridge.pkl','rb'))
@@ -32,7 +33,7 @@ def predict_datapoint():
         result = ridge_model.predict(new_data_scaled)
 
         return render_template('home.html',result=result[0])
-    else:
+    else: 
         return render_template('home.html')
     
 
